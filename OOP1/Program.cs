fa8b3cdea02362aff93eeee535a281a065247bfe
= <baran.dogan19007@gmail.com>
2021-02-19 18:13:43 +0300

    Ders sonu

---
+++ OOP1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun product = new Urun();
+            product.Id = 1;
+            product.CategoryId = 2;
+            product.ProductName = "Masa";
+            product.UnitPrice = 500;
+            product.UnitsInStock = 3;
+
+            Urun product2 = new Urun { Id = 2, CategoryId = 5, 
+                                       ProductName = "Kalem", UnitPrice = 45, 
+                                       UnitsInStock = 9 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product);
+        }
+    }
+}
